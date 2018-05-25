1451e19503710ba38356a2711457ff60457b346b
web38 <web38@PC-S821>
2018-05-25 17:45:47 +0200

    segundo comentario carlos

--- php/muro.php
+++ php/muro.php
@@ -8,6 +8,7 @@
         //nuevo comentario carlos acevedo   
        //creamos la consulta de seleccion del tag y le damos formato 
        //JSON Y LA RETORNAMOS
+     //linea 5 Carlos Acevedo
      
     $consulta="SELECT * FROM Tags order by Fecha DESC";
 
