2459acc130b8e269a4378bc8a21b7ba5f08b3da5
rotulance <>
2018-05-28 18:36:07 +0200

    Cambio Javi fichero Nuevo

--- COMENTARIOS/mirarlikes.php
+++ COMENTARIOS/mirarlikes.php
@@ -3,7 +3,7 @@ header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
 if (session_id() === "") { session_start(); }
 
-include '../lib/lib1.php';
+//include '../lib/lib1.php';
   conectarBD();
   $consulta="SELECT * FROM Likes";
   $resultado=$conexion->query($consulta);
