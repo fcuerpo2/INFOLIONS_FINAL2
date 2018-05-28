1c5739bfadbfb88274901acaec4b11e9e2584ac1
rotulance <>
2018-05-28 19:26:55 +0200

    Ok

--- COMENTARIOS/mirarlikes.php
+++ COMENTARIOS/mirarlikes.php
@@ -2,17 +2,13 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
 if (session_id() === "") { session_start(); }
-
+$_SESSION['TodosLikes'] = "";
 //include '../lib/lib1.php';
   conectarBD();
   $consulta="SELECT * FROM Likes";
   $resultado=$conexion->query($consulta);
-  if($resultado->num_rows>0){
-    //HAY ALGÚN USUARIO 
-    $fila=mysqli_fetch_assoc($resultado);    
-  }else{
-    //NO HAY NINGÚN USUARIO
-    //  header('location:../index.php');
+  while ($fila = mysql_fetch_row($resultado)) {         
+        $_SESSION['TodosLikes'][] = $fila;
   }
   desconectarBD();
 ?>
