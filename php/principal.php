9df144e3d00f416cb4ecae73fb2a17447e251542
rotulance <>
2018-05-28 20:46:32 +0200

    Ok

--- php/principal.php
+++ php/principal.php
@@ -106,6 +106,7 @@ echo "<div class='texto'>".$miArray[$i]['Texto']."</div>";
 echo "<div class='imagenes'></div>";
 echo "<div class='botones' style='margin-top:10px;'>";
 $totalLikes = 0;
+$encontrado="NO";
 for($z=0;$z<count($_SESSION['TodosLikes']);$z++)
 {
     if ($_SESSION['TodosLikes'][$z]['IdComentario'] == $miArray[$i]['idTag'])
@@ -113,14 +114,21 @@ for($z=0;$z<count($_SESSION['TodosLikes']);$z++)
         $totalLikes++;
         if ($_SESSION['TodosLikes'][$z]['IdUsuarioEnvia'] == $miArray[$i]['idUsuario'])
         {
-            echo "<input type='button' class='btn btn-primary' value='Me gusta'/>&nbsp;&nbsp;";           
+            $encontrado="SI";
         }
         else
         {
-            echo "<input type='button' class='btn btn-primary' value='Me puede gustar'/>&nbsp;&nbsp;";
+            $encontrado="NO";
         }
-    }
-        
+    }        
+}
+if ($encontrado == "NO")
+{
+        echo "<input type='button' class='btn btn-primary' value='Me puede gustar'/>&nbsp;&nbsp;";
+}
+else
+{
+        echo "<input type='button' class='btn btn-primary' value='Me gusta'/>&nbsp;&nbsp;";               
 }
 echo "<input type='button' class='btn btn-primary' value='Comentario'/>";
 echo "</div>";
