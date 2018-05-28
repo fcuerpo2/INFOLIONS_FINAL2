9d82a4c121428248aa395d63608f496b9af849ba
rotulance <>
2018-05-28 19:09:21 +0200

    Ok

--- js/script1.js
+++ js/script1.js
@@ -157,9 +157,9 @@ function mostrarTags(obj){
 function crearTags(miTexto,index){
 	var idTag=miTexto.idTag;
    t+="<div class='tag'><div class='cabecera'><img src='../doc/fotoportada/"+miTexto.FotoPortada+"' onclick='verImagen("+miTexto.FotoPortada+")  "+miTexto.Nombre+" "+miTexto.Apellidos+" "+miTexto.Fecha+"</div>";
-   t+="<div class='titulo' style='padding-bottom:3px;'>"+miTexto.Cabecera+"</div>";
-   t+="<div class='texto' style='padding-bottom:3px;'>"+miTexto.Texto+"</div>";
-   t+="<div id='imagenes'></div><div id='botones' style='margin-top:5px;'><input type='button' class='btn btn-primary' value='Me gusta'/><input type='button' class='btn btn-primary' value='Comentario'/></div>";
+   t+="<div class='titulo' style='margin-bottom:3px;'>"+miTexto.Cabecera+"</div>";
+   t+="<div class='texto' style='margin-bottom:3px;'>"+miTexto.Texto+"</div>";
+   t+="<div id='imagenes'></div><div id='botones' style='margin-top:5px;'><input type='button' class='btn btn-primary' value='Me gusta'/>&nbsp;&nbsp;<input type='button' class='btn btn-primary' value='Comentario'/></div>";
    t+="<div id='comentarios'></div></div>"; 
 }
 
