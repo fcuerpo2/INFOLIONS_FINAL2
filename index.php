d4a53dc60fd2f734d121cf70dfc69e2dd5984b9f
rotulance <39596581+rotulance@users.noreply.github.com>
2018-05-28 16:27:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
-if (session_id() == "") 
-    session_start(); 
-
+if (session_id() === "") { session_start(); }
 ?>
 <html lang="es">
 <head>
@@ -23,36 +21,29 @@ if (session_id() == "")
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 	<meta charset="UTF-8">
 	    <!-- Custom styles for this template -->
-    <link href="./css/estilo1.css" rel="stylesheet">
-    <script src='./js/script1.js' type='text/javascript'></script>
+    	<link href="./css/estilo1.css" rel="stylesheet">
+    	<script src='./js/script1.js' type='text/javascript'></script>
 </head>
 <body class="login">
-
-    <div id='acceso' class='tag'>
-
-    <form class="form-signin" action="./php/login.php" method="POST">
-      <img class="mb-4 fotologin" src="./img/reservoir.png" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Iniciar Sesión</h1>
-      <label for="inputEmail" class="sr-only">Email</label>
-      <input type="email" name="email" id="email" class="form-control" placeholder="Email" required autofocus>
-      <label for="inputPassword" class="sr-only">Contraseña</label>
-      <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Recuérdame
-        </label>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Inicia Sesión</button>
-      <p class="mt-5 mb-3 text-muted">Si no tienes cuenta, <a href='#' onclick='registrate();'>regístrate</a></p>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
-
+<div class="container-fluid">
+    <div id='acceso' class='tag2'>
+    	<form class="form-signin" action="./php/login.php" method="POST">
+      		<img class="mb-4 fotologin" src="./img/reservoir.png" alt="" width="72" height="72">
+      		<h1 class="h3 mb-3 font-weight-normal">Iniciar Sesión</h1>
+      		<label for="inputEmail" class="sr-only">Email</label>
+      		<input type="email" name="email" id="email" class="form-control" placeholder="Email" required autofocus>
+      		<label for="inputPassword" class="sr-only">Contraseña</label>
+      		<input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+      		<div class="checkbox mb-3">
+        		<label>
+          			<input type="checkbox" value="remember-me"> Recuérdame
+        		</label>
+      		</div>
+      		<button class="btn btn-lg btn-primary btn-block" type="submit">Inicia Sesión</button>
+      		<p class="mt-5 mb-3 text-muted">Si no tienes cuenta, <a href='#' onclick='registrate();'>regístrate</a></p>
+      		<p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    	</form>
     </div>
-
-
-
-
-
-
+</div>
 </body>
 </html>
