153f8a5f668fb4112adf808b0f77818e04534534
rotulance <>
2018-05-29 13:21:13 +0200

    Ok

--- php/principal.php
+++ php/principal.php
@@ -64,7 +64,7 @@ echo "
             <span class='icon-bar'></span>
             <span class='icon-bar'></span>
             </button>
-            <a class='navbar-brand' href='#'>INFOLIONS</a>
+            <a class='navbar-brand' href='#'><img src='../img/reservoir.png' alt='InfoLions' title='InfoLions' style='height:45px;'>   INFOLIONS</a>
         </div>
         <div class='collapse navbar-collapse' id='myNavbar'>
             <ul class='nav navbar-nav'>
