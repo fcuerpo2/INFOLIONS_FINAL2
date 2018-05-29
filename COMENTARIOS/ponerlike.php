3ffc7f0e77d693cd7cc2c4fcf0aaf3c01817fd65
rotulance <>
2018-05-29 19:40:13 +0200

    Ok

--- COMENTARIOS/ponerlike.php
+++ COMENTARIOS/ponerlike.php
@@ -2,6 +2,7 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
 if (session_id() === "") { session_start(); }
+include '../lib/lib1.php';
 $_SESSION['TodosLikes'] = "";
 //include '../lib/lib1.php';
   conectarBD();
