75e438cfa58c08e60eb87f0e43afc174015bf00b
rotulance <>
2018-05-30 09:51:13 +0200

    Ok

--- chat/config.php
+++ chat/config.php
@@ -2,10 +2,10 @@
 if(!isset($dbh)){
  session_start();
  date_default_timezone_set("UTC");
- $musername = "remoto10";
- $mpassword = "!RotBacosoft!4";
- $hostname  = "localhost";
- $dbname    = "chatting";
+ $musername = "qzw186";
+ $mpassword = "Qwerty1";
+ $hostname  = "qzw186.formadin.com";
+ $dbname    = "qzw186";
  $dbh=new PDO('mysql:dbname='.$dbname.';host='.$hostname.";port=3306",$musername, $mpassword);
  /*Change The Credentials to connect to database.*/
  include("../chat/user_online.php");
