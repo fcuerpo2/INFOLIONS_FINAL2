8256b1764d8b6a053bd82ace49cd48a4489b43ba
rotulance <web38@PC-S812>
2018-05-30 18:03:40 +0200

    Ok

--- js/script1.js
+++ js/script1.js
@@ -203,5 +203,13 @@ function ponerlike(numTag,UserEnvia,UserRecibe)
 
 function quitarlike(numTag,UserEnvia,UserRecibe)
 {
-    alert("Quitar Like");
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Botones-"+numTag).innerHTML = this.responseText;
+        }
+    }
+    xhttp.open("GET", "../COMENTARIOS/quitarlike.php?NumTag="+numTag+"&UserEnvia="+UserEnvia+"&UserRecibe="+UserRecibe, true);
+    xhttp.send();    
 }
