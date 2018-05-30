ff88913900ae818360c9cf2424afb9738b112e8f
rotulance <>
2018-05-30 18:58:24 +0200

    Ok

--- js/script1.js
+++ js/script1.js
@@ -188,7 +188,7 @@ function mostrarTags(obj){
 
 function crearTags(miTexto,index){
 	var idTag=miTexto.idTag;
-   t+="<div class='tag'><div class='cabecera'><img src='../doc/fotoportada/"+miTexto.FotoPortada+"' onclick='verImagen("+miTexto.FotoPortada+")' class='escalar'>&nbsp;&nbsp;&nbsp;&nbsp;"+miTexto.Nombre+" "+miTexto.Apellidos+" "+miTexto.Fecha+"</div>";
+   t+="<div id='Tag-"+miTexto.idUsuario+"' class='tag sombranegra'><div class='cabecera'><img src='../doc/fotoportada/"+miTexto.FotoPortada+"' onclick='verImagen("+miTexto.FotoPortada+")' class='escalar'>&nbsp;&nbsp;&nbsp;&nbsp;"+miTexto.Nombre+" "+miTexto.Apellidos+" "+miTexto.Fecha+"</div>";
    t+="<div class='titulo' style='padding-bottom:3px;'>"+miTexto.Cabecera+"</div>";
    t+="<div class='texto' style='padding-bottom:3px;'>"+miTexto.Texto+"</div>";
    t+="<div id='imagenes'></div><div id='botones' style='margin-top:10px;'><input type='button' class='btn btn-primary' value='Me gusta'/>&nbsp;&nbsp;&nbsp;<input type='button' class='btn btn-primary' value='Comentario'/></div>";
