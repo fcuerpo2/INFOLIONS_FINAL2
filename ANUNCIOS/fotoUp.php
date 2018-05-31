a1a7c82915115494054932717e7539faee9892e1
web38 <web38@PC-S821>
2018-05-31 20:13:46 +0200

    metodo js para anuncios AJAX

--- ANUNCIOS/fotoUp.php
+++ ANUNCIOS/fotoUp.php
@@ -3,23 +3,23 @@
   include '../lib/lib1.php';
 
 
-  if(isset($_FILES['fotoportada'])){
+  if(isset($_FILES['fotoAnuncio'])){
   
-  $foto=$_SESSION['usu']['idUsuario']."-".$_FILES['fotoportada']['name'];
+    $foto=$_SESSION['usu']['idUsuario']."-".$_FILES['fotoAnuncio']['name'];
 
-move_uploaded_file($_FILES['fotoportada']['tmp_name'],"../doc/fotosPublicidad/$foto");
+    move_uploaded_file($_FILES['fotoAnuncio']['tmp_name'],"../doc/fotosPublicidad/$foto");
 
-  $_SESSION['usu']['FotoPortada']=$foto;
-//TODO: colocar código relacionar foto con foto subida
-  /*
-  conectarBD();
+    $_SESSION['anuncio']['fotoAnuncio']=$foto;
+    //TODO: colocar código relacionar foto con foto subida
+    /*
+    conectarBD();
     $consulta="UPDATE usuarios SET FotoPortada='$foto' WHERE idUsuario=".$_SESSION['usu']['idUsuario'];
 
-  $resultado=$conexion->query($consulta);
-  desconectarBD();*/
+    $resultado=$conexion->query($consulta);
+    desconectarBD();*/
 
-  //echo $foto;
-  echo "salida a piñon";
+    echo $foto;
+ 
     }
-    echo 'no hay fotoportada'
+    echo 'no hay foto valida en fotoUp.php'
 ?>
