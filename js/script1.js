9fb81d8a3444ff3a0f443d17d348dc5559dbfc2b
rotulance <>
2018-05-31 18:34:30 +0200

    Ok

--- js/script1.js
+++ js/script1.js
@@ -169,16 +169,13 @@ function registrar(){
 
         }
     });
-       
+ if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(showPosition);
+  }    
  }
 
   function enviartag(){
 
- if (navigator.geolocation){
-//  navigator.geolocation.getCurrentPosition(showPosition);
-  }else{
-
-  }
       document.getElementById("botPubliTag").value="Publicando...";
       var tags="";
       var formdata = new FormData($('#ftag')[0]);
@@ -200,7 +197,7 @@ function registrar(){
 
  }
 
-  function showPosition(){
+  function showPosition(position){
   document.getElementById('latitud').value=position.coords.latitude;
   document.getElementById('longitud').value=position.coords.longitude;
  }
