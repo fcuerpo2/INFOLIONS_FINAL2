89902fd7778a6be0d37867f7f31aafce0278df52
web38 <web38@PC-S821>
2018-06-01 20:25:30 +0200

    metodo js para anuncios AJAX

--- ANUNCIOS/recuperarAnuncioPersistencia.php
+++ ANUNCIOS/recuperarAnuncioPersistencia.php
@@ -1,5 +1,38 @@
 <?php
+    if (session_id() == "") session_start(); 
+    include '../lib/lib1.php';
+    $anuncio="A LA ESPERA DE NUEVOS ANUNCIOS";
+    
+    //select id de anuncios en array
+    //mostrar datos de uno de los anuncios de forma aleatoria
+    if( $_SESSION['usu']['idUsuario']!=""){
+       
+        $coleccionIdAnuncios="SELECT idAnuncio FROM publicidad";
+        $selAnuncio = "SELECT * FROM publicidad WHERE idAnuncio=$elegido";
+        conectarBD();
+        $arrayIdAnuncios = array();
 
-
-
+        if ($resultado= $conexion->query($coleccionIdAnuncios)) {
+            while($row = $resultado->fetch_assoc()) {
+                    $arrayIdAnuncios[] = $row;
+            }
+            $elegido=rand(0, count($arrayIdAnuncios));
+             if ($resultado= $conexion->query($selAnuncio)){
+                 while($row = $resultado->fetch_assoc()) {
+                      
+                     $anuncio=$row;
+                }
+             }
+            
+        }
+        desconectarBD();
+        
+        
+        echo $anuncio;
+    
+    
+    }else{
+     echo $anuncio;   
+    }
+    
 ?>
