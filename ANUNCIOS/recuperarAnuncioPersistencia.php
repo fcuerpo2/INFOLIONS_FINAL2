0bb8464d6bcf97831e8df8b195fffd29b6696ecc
web38 <web38@PC-S821>
2018-06-04 17:00:08 +0200

    metodo js para anuncios AJAX

--- ANUNCIOS/recuperarAnuncioPersistencia.php
+++ ANUNCIOS/recuperarAnuncioPersistencia.php
@@ -1,14 +1,14 @@
 <?php
     if (session_id() == "") session_start(); 
     include '../lib/lib1.php';
-    $anuncio="A LA ESPERA DE NUEVOS ANUNCIOS";
+    $anuncio="<div id='contenido-anuncio'><h1>A LA ESPERA DE NUEVOS ANUNCIOS</h1></div>";
     
     //select id de anuncios en array
     //mostrar datos de uno de los anuncios de forma aleatoria
     if( $_SESSION['usu']['idUsuario']!=""){
        
         $coleccionIdAnuncios="SELECT idAnuncio FROM publicidad";
-        $selAnuncio = "SELECT * FROM publicidad WHERE idAnuncio=$elegido";
+        
         conectarBD();
         $arrayIdAnuncios = array();
     $_SESSION['anuncio']['recuperado']="nada";
@@ -17,14 +17,16 @@
             while($row = $resultado->fetch_assoc()) {
                     $arrayIdAnuncios[] = $row;
             }
-            $elegido=rand(0, count($arrayIdAnuncios));
-           
-             if ($resultado= $conexion->query($selAnuncio)){
-                 $_SESSION['anuncio']['recuperado']=$elegido;
-                 while($row = $resultado->fetch_assoc()) {
+            $elegido=rand(1, count($arrayIdAnuncios));
+            $selAnuncio = "SELECT * FROM publicidad WHERE idAnuncio=$elegido";
+             $_SESSION['anuncio']['recuperado']=$selAnuncio;
+
+             if ($resultado2= $conexion->query($selAnuncio)){
+                
+                 while($row = $resultado2->fetch_assoc()) {
                       //TODO: extraer del row el valor
-                     $anuncio=$elegido;
-                     $_SESSION['anuncio']['recuperado']=$elegido;
+                     //$anuncio.=$elegido;
+                     $_SESSION['anuncio']['recuperado']=$row;
                 }
              }
             
