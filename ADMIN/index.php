c0e7ec7491f7a35e06a0987628b5cdbbcb54399e
web38 <web38@PC-S824>
2018-06-04 17:08:09 +0200

    Betty con Javi

--- ADMIN/index.php
+++ ADMIN/index.php
@@ -2,6 +2,10 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
 if (session_id() === "") { session_start(); }
+if ($_SESSION['usu']['TipoUsuario'] == 1)
+{
+    header('location:../ADMIN/principal.php');
+}
 ?>
 <html lang="es">
 <head>
