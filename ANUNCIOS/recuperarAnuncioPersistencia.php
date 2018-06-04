b9dd2d282fc229a9a0260472e584154a954546de
web38 <web38@PC-S821>
2018-06-04 18:00:44 +0200

    metodo js para anuncios AJAX

--- ANUNCIOS/recuperarAnuncioPersistencia.php
+++ ANUNCIOS/recuperarAnuncioPersistencia.php
@@ -17,16 +17,30 @@
             while($row = $resultado->fetch_assoc()) {
                     $arrayIdAnuncios[] = $row;
             }
-            $elegido=rand(1, count($arrayIdAnuncios));
-            $selAnuncio = "SELECT * FROM publicidad WHERE idAnuncio=$elegido";
-             $_SESSION['anuncio']['recuperado']=$selAnuncio;
+            $elegidos = array();
+            $selAnuncio = array();
+            
+            for($x = 0; $x < 3; $x++) {
+                $elegidos[$x]=rand(1, count($arrayIdAnuncios));
+                $selAnuncios[$x] = "SELECT * FROM publicidad WHERE idAnuncio=$elegidos[0]";
+            }
+            
+            
+            $_SESSION['anuncio']['recuperado']=$selAnuncios[0];
 
-             if ($resultado2= $conexion->query($selAnuncio)){
+             if ($resultado2= $conexion->query($selAnuncios[0])){
                 
                  while($row = $resultado2->fetch_assoc()) {
                       //TODO: extraer del row el valor
                      //$anuncio.=$elegido;
                      $_SESSION['anuncio']['recuperado']=$row;
+                     $anuncio = $_SESSION['anuncio']['recuperado']['titulo'].", "
+                             .$_SESSION['anuncio']['recuperado']['descripcion'].", "
+                             .$_SESSION['anuncio']['recuperado']['imagen'];
+                     $_SESSION['anuncio']['salida']=$anuncio;
+                    // echo "<div id='anuncio-top' style='background-color: #0e5f0e; margin-bottom: 20px; border-radius: 10px; padding: 10px; color: #fff;' class='sombraNegra'>Que anuncio</div>";
+                     echo $anuncio;
+                     
                 }
              }
             
@@ -34,7 +48,7 @@
         desconectarBD();
         
         
-        echo $anuncio;
+       // echo $anuncio;
     
     
     }else{
