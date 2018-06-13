d2af6fecfddcd23c4e8684780d3c6b9ddad94985
web38 <web38@PC-S821>
2018-06-13 18:59:38 +0200

    Anuncios con BootStrap el top arriba, subsanado bug de anuncio en blanco cada tanto

--- php/login.php
+++ php/login.php
@@ -15,17 +15,17 @@ if (session_id() === "") { session_start(); }
   // 1: poniendo un correro con formato valido y como password 'or '1'='1
   //  2: detrás del 'or '1'='1 podemos poner ; DROP TABLE usuarios; SELECT * FROM datos WHERE nombre LIKE '%';
  // $consulta="SELECT * FROM usuarios WHERE Email='$email' AND password='$password'";
-  //NO FUNCIONA mysql_real_escape_string($email) ."' AND password='". mysql_real_escape_string($password)."'";
-  //$consulta = sprintf("SELECT * FROM usuarios WHERE Email='%s' AND password='%s'", $email, $paswword);
+  //OTRA OPCION, PERO NO FUNCIONA mysql_real_escape_string($email) ."' AND password='". mysql_real_escape_string($password)."'";
+  //FELIPE LA PROBÓ AÑADIENDO COMO ATRIBUTO LA CONEXION $consulta = sprintf("SELECT * FROM usuarios WHERE Email='%s' AND password='%s'", $email, $paswword);
   
-  
+  //Método-2: No funcionan las inyecciones SQL
   $stmt = $conexion->prepare("SELECT * FROM usuarios WHERE Email=? AND password=? LIMIT 1");
   $stmt->bind_param('ss', $email, $password);
   $stmt->execute();
- 
   $resultado = $stmt->get_result();
   
- //Metodo con real_escape_string (que no va)  o sin  
+ //Método-1 
+ //
  //$resultado=$conexion->query($consulta);
  $_SESSION['resultado']= $resultado;
   if($resultado->num_rows>0){
