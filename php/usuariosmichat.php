c4150a9731f6823d103daeb14275616b91957f9d
rotulance <>
2018-06-14 18:32:41 +0200

    Ok

---
+++ php/usuariosmichat.php
@@ -0,0 +1,27 @@
+<?php
+if (session_id() === '') { session_start(); }
+
+if($_SESSION['usu']['idUsuario']!=""){
+
+  $_SESSION['TodosUsuariosMiChat']="";  
+  conectarBD();
+  $consulta="SELECT * FROM chatters";
+  $resultado=$conexion->query($consulta);
+  
+  $_SESSION['TodosUsuariosMiChat'] = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
+  
+  desconectarBD();
+  
+  $nombre=$_SESSION['usu']['Nombre'];
+  $apellidos=$_SESSION['usu']['Apellidos'];
+  $foto=$_SESSION['usu']['FotoPortada'];
+  $fecha=date('d-m-y h:i:sa');
+  for ($numusuarios=0;$numusuarios<count($_SESSION['TodosUsuariosMiChat']);$numusuarios++)
+  {
+            echo "<span style='color:#000; text-align:center;'><strong>".utf8_decode($_SESSION['TodosUsuariosMiChat'][$numusuarios]['name'])."</strong></span><br/>";
+  }            
+}
+else{
+  header("location: ../index.php");
+}
+?>
